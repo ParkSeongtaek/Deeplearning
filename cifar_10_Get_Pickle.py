0e69aed88fd060ae98ecb7eced3b3e92e4ee137c
ParkSeongtaek <56757197+ParkSeongtaek@users.noreply.github.com>
2022-11-21 21:15:08 +0900

    Update cifar_10_Get_Pickle.py

--- cifar_10_Get_Pickle.py
+++ cifar_10_Get_Pickle.py
@@ -4,9 +4,10 @@ from PIL import Image
 import pickle
 import random
 import natsort ## 숫자 정렬용 라이브러리
+from einops import rearrange, repeat, reduce
 #변환할 이미지 목록 불러오기
 
-
+'''
 LABELS = ['frog', 'truck', 'deer', 'automobile', 'bird', 'horse', 'ship',
           'cat', 'dog', 'airplane']
 
@@ -26,27 +27,46 @@ img_list_np = []
 img_list_np = []
 idx = 0
 for i in img_list_png:
-        label_table = [0 for i in range(10)]
-        label_table[LABELS.index(label[idx])] = 1 
-        #print(label_table)
-        
-        img = Image.open(image_path + i)        #Image형태로 열기
-        #print(type(img))
-        #img.show()
-        img_array = np.array(img)               #np형태로 변환
-
-        img_array = img_array.ravel()             #DNN을 위한 Data 1차원으로 변환
-        #print(img_array.shape)
-        img_tuple = (img_array,label_table)
-        
-        img_list_np.append(img_tuple)           
-        #print(i, " 추가 완료 - 구조:", img_array.shape) # 불러온 이미지의 차원 확인 (세로X가로X색)
-        #print(img_array.T.shape) #축변경 (색X가로X세로)
-        idx += 1
+    #label_table = [0 for i in range(10)]
+    #label_table[LABELS.index(label[idx])] = 1 
+    label_table = LABELS.index(label[idx]) 
+    
+    #print(label_table)
+    
+    img = Image.open(image_path + i)        #Image형태로 열기
+    #print(type(img))
+    #img.show()
+    img_array = np.array(img)               #np형태로 변환
+    
+    #img_array = img_array.ravel()             #DNN을 위한 Data 1차원으로 변환
+    
+    img_array = rearrange(img_array, 'r h C -> C r h')
+    img_array = np.array(img_array)/255
+    #print(img_array.shape)
+    img_tuple = (img_array,label_table)
+    img_list_np.append(img_tuple)           
+    #print(i, " 추가 완료 - 구조:", img_array.shape) # 불러온 이미지의 차원 확인 (세로X가로X색)
+    #print(img_array.T.shape) #축변경 (색X가로X세로)
+    idx += 1
+    if idx%100 == 0:
+        print(idx/100)
 print (img_list_np.__len__())
 
-with open("CIRFAR_10_DNN_32.pickle","wb") as fw:
+
+
+with open("CIRFAR_10_CNN_32.pickle","wb") as fw:
     pickle.dump(img_list_np, fw)
+
+'''
+with open("CIRFAR_10_CNN_32.pickle","rb") as fr:
+    img_list_np = pickle.load(fr)
+
+
+print(img_list_np.__len__())
+for idx in range(1):
+    print(img_list_np[idx][0])
+    print(img_list_np[idx][1])
+
 
 
 
